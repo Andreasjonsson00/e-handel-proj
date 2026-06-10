83d13b983091fddc00ad4e454bbe27b5c085242e
Andreas <andreasjonsson00@gmail.com>
2026-06-10 12:43:57 +0200

    add PostgreSQL database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,42 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+import cors from "cors";
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API fungerar!');
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM products`,
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching products");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servern körs på port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
