bc4e244bcb5a62e7fadabac7424a018fbdc4c756
Andreas <andreasjonsson00@gmail.com>
2026-08-18 16:26:41 +0200

    feat: add login page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Frontpage from "./pages/Frontpage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Frontpage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
