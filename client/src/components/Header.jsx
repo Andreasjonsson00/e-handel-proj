2412045016940791862f40fb3b42c22b6d3be941
Andreas <andreasjonsson00@gmail.com>
2026-08-19 14:19:43 +0200

    add header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+ const Header = ({ auth, onLogout }) => {
+
+  return (
+    <>
+      <header className="site-header">
+        {" "}
+        <h1>Min e-handel</h1> {auth && <strong>{auth.user.role}</strong>}{" "}
+        <nav>
+          {" "}
+          {auth?.user.role === "admin" && <Link to="/admin">Admin</Link>}{" "}
+          {auth ? (
+            <button type="button" onClick={onLogout}>
+              {" "}
+              Logga ut{" "}
+            </button>
+          ) : (
+            <Link to="/login">Logga in</Link>
+          )}{" "}
+        </nav>{" "}
+      </header>
+    </>
+  );
+};
+
+export default Header;
