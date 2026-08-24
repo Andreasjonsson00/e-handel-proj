3e0811230e0753559875031cb4ec19721e4780c2
Andreas <andreasjonsson00@gmail.com>
2026-08-24 17:31:04 +0300

    feat: orders page nav button in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 
- const Header = ({ auth, onLogout }) => {
-
+const Header = ({ auth, onLogout }) => {
   return (
     <>
       <header className="site-header">
@@ -9,7 +8,11 @@ import { Link } from "react-router-dom";
         <h1>Min e-handel</h1> {auth && <strong>{auth.user.role}</strong>}{" "}
         <nav>
           {" "}
-          {auth?.user.role === "admin" && <Link to="/admin">Admin</Link>}{" "}
+          {auth?.user.role === "admin" && (
+            <Link to="/orders">
+              <button type="button">Ordrar</button>
+            </Link>
+          )}{" "}
           {auth ? (
             <button type="button" onClick={onLogout}>
               {" "}
